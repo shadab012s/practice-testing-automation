2d1c5e13c5ef5f3a3f66e5e9100f373b909bbe20
shadab012s <mdshadab0012s@gmail.com>
2025-08-29 17:54:47 +0530

    Initial commit

---
+++ test.py
@@ -0,0 +1,25 @@
+import re
+from playwright.sync_api import Playwright, sync_playwright, expect
+
+
+def run(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch(headless=False, slow_mo=500) 
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("https://shadab012s.github.io/PRACTISE/")
+    page.get_by_role("textbox", name="Full Name").click()
+    page.get_by_role("textbox", name="Full Name").fill("shsh")
+    page.get_by_role("textbox", name="Email").click()
+    page.get_by_role("textbox", name="Email").fill("sh@yopmail.com")
+    page.get_by_role("textbox", name="Department").click()
+    page.get_by_role("textbox", name="Department").fill("admin")
+    page.get_by_label("Role").select_option("admin")
+    page.get_by_role("button", name="Submit").click()
+
+    # ---------------------
+    context.close()
+    browser.close()
+
+
+with sync_playwright() as playwright:
+    run(playwright)
